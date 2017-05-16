a63905ad4aa292df33abfb3610daf60932338ba9
15039836 <15039836@myrp.edu.sg>
2017-05-16 23:54:53 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,13 +1,70 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNote;
+    Button btnInsert, btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNote = (EditText)findViewById(R.id.editTextNote);
+        btnInsert = (Button)findViewById(R.id.buttonInsertNote);
+        btnShow = (Button)findViewById(R.id.buttonShowList);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Get the RadioGroup object
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroupStars);
+                // Get the Id of the selected radio button in the RadioGroup
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                // Get the radio button object from the Id we had gotten above
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                String theid = rb.getText().toString();
+                //String theNum = getResources().getString(rb.getText());
+                int num = Integer.parseInt(theid);
+
+                // Insert a task
+                db.insertNote(etNote.getText().toString(), num);
+                db.close();
+
+                //Show a Toast that display the text on the selected radio button
+                Toast.makeText(getBaseContext(), "Inserted",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+
+//                String[] info = {currentInfo.getFirstName(), currentInfo.getLastName(), currentInfo.getPhone(),
+//                        currentInfo.getCountry(), currentInfo.getCity(), currentInfo.getPostalCode(),
+//                        currentInfo.getAddress()};
+//
+//                i.putExtra("theId", info);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -2,13 +2,29 @@ package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
+
+	ListView lv;
+	ArrayAdapter aa;
+	ArrayList<Note> noteList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//TODO implement the Custom ListView
+
+
+		DBHelper db = new DBHelper(SecondActivity.this);
+		noteList = db.getNoteContent();
+		db.close();
+
+		aa = new RevisionNotesArrayAdapter(this, R.layout.row, noteList);
+		lv.setAdapter(aa);
 
 	}
 
